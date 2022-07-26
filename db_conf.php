38e825fe024b086915cdb5caddde4a7f4554ff37
najibbaraka <najib.baraka@epitech.eu>
2022-07-26 14:21:46 +0200

    config de base

--- db_conf.php
+++ db_conf.php
@@ -1,6 +1,11 @@
 <!-- mysql://b321f979a60179:0e527312@eu-cdbr-west-03.cleardb.net/heroku_1ab008876e3f8df?reconnect=true -->
 <?php
-$host = "eu-cdbr-west-03.cleardb.net";
-$dbname = "heroku_1ab008876e3f8df";
-$user = "b321f979a60179";
-$password = "0e527312";
+// $host = "eu-cdbr-west-03.cleardb.net";
+// $dbname = "heroku_1ab008876e3f8df";
+// $user = "b321f979a60179";
+// $password = "0e527312";
+
+$host = "localhost";
+$dbname = "cinema";
+$user = "root";
+$password = "root";
